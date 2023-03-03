990b8f38da59015d4a3a83f78559a06ba65d6e47
maxxie114 <maxxie114@gmail.com>
2023-03-03 11:24:26 +0000

    Enable debug for now

--- pcibex-clone/data_includes/main.js
+++ pcibex-clone/data_includes/main.js
@@ -1,6 +1,7 @@
 PennController.ResetPrefix(null); // Shorten command names (keep this line here)
 
-PennController.DebugOff();
+// Enable debug for now
+// PennController.DebugOff();
 
 var counterOverride = 5;
 
@@ -8,14 +9,14 @@ var counterOverride = 5;
 var showProgressBar = true;
 
 var shuffleSequence = seq(
-    'consent',
-    'practice1',
-    'practice2',
-    'practice3',
-    'shared-intro',
-    sepWith("timeoutSep", rshuffle(startsWith("PREFGm"),startsWith("pozzi"),startsWith("filledgap"),startsWith("STIMPY"))),
-    'debrief'
-    );
+  'consent',
+  'practice1',
+  'practice2',
+  'practice3',
+  'shared-intro',
+  sepWith("timeoutSep", rshuffle(startsWith("PREFGm"), startsWith("pozzi"), startsWith("filledgap"), startsWith("STIMPY"))),
+  'debrief'
+);
 
 // Using modified controller coded by Ethan Poole (Umass, 2017)
 // Disallows use of mouse for responses.
@@ -28,58 +29,62 @@ var completionErrorMessage = "There was an error in sending your data to the ser
 
 // Controller settings.
 var defaults = [
-    "Question", {
-        hasCorrect: true,
-        presentHorizontally: false
-},
-    "EPDashedSentence", {
-        mode: "speeded acceptability",
-        display: "in place"
-}
+  "Question", {
+    hasCorrect: true,
+    presentHorizontally: false
+  },
+  "EPDashedSentence", {
+    mode: "speeded acceptability",
+    display: "in place"
+  }
 ];
 
 // Items array.
 var items = [
 
-["consent", "Form", {consentRequired: true, html: {include: "consent.html"}}],
-["intro", "Form", {consentRequired: true, html: {include: "intro.html"}}],
-["debrief", "Form", {consentRequired: true, html: {include: "debrief.html"}}],
+  ["consent", "Form", { consentRequired: true, html: { include: "consent.html" } }],
+  ["intro", "Form", { consentRequired: true, html: { include: "intro.html" } }],
+  ["debrief", "Form", { consentRequired: true, html: { include: "debrief.html" } }],
 
-['shared-intro', "Form", {consentRequired: false, html: {include: "practice1.html"}}],
-['shared-intro', "Form", {consentRequired: false, html: {include: "practice2.html"}}],
-['shared-intro', "Form", {consentRequired: false, html: {include: "practice3.html"}}],
+  ['shared-intro', "Form", { consentRequired: false, html: { include: "practice1.html" } }],
+  ['shared-intro', "Form", { consentRequired: false, html: { include: "practice2.html" } }],
+  ['shared-intro', "Form", { consentRequired: false, html: { include: "practice3.html" } }],
 
 
-['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Wrong answer. Please read slowly and carefully."}, DS, {s: "This sentence is to get you used to the reading style."}],
-['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Wrong answer. Please read slowly and carefully."}, DS, {s: "This is another sentence, which is a bit more complicated than the one that came before it."}],
+  ['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Wrong answer. Please read slowly and carefully." }, DS, { s: "This sentence is to get you used to the reading style." }],
+  ['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Wrong answer. Please read slowly and carefully." }, DS, { s: "This is another sentence, which is a bit more complicated than the one that came before it." }],
 
-['shared-intro', Message, {consentRequired: false,
-                   html: ["div",
-                           ["p", "How was that? It is important that you move through the sentence carefully and deliberately, making sure that you understand each word as you go."],
-                           ["p", "Let's try some more."]
-                         ]}],
+  ['shared-intro', Message, {
+    consentRequired: false,
+    html: ["div",
+      ["p", "How was that? It is important that you move through the sentence carefully and deliberately, making sure that you understand each word as you go."],
+      ["p", "Let's try some more."]
+    ]
+  }],
 
-['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Not the answer we were expecting. Be sure to read carefully."}, DS, {s: "At the ball, the prince waltzed and slowly smiled at the princess."}, Question, {q: "On the prince's face was a ___.", as: ["smile", "grimace"]}],
-['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Not the answer we were expecting. Be sure to read carefully."}, DS, {s: "Without warning, Geoffrey turned and screamed at the waiter who had embarrassed him."}, Question, {q: "The waiter ___.", as: ["embarrassed Geoffrey", "felt embarrassed by Geoffrey"]}],
-['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Not the answer we were expecting. Be sure to read carefully."}, DS, {s: "The trainee talking to the barista lazily made a latte and didn't even try to make a design on top."},Question, {q: "The trainee was probably ___.",  as: ["not very attentive", "following procedure"]}],
-['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Not the answer we were expecting. Be sure to read carefully."}, DS, {s: "Madame de Plessy sat up all night worrying about her son who was on the front lines."},Question, {q: "Madame de Plessy was concerned about ___.", as: ["her son", "her husband"]}],
+  ['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Not the answer we were expecting. Be sure to read carefully." }, DS, { s: "At the ball, the prince waltzed and slowly smiled at the princess." }, Question, { q: "On the prince's face was a ___.", as: ["smile", "grimace"] }],
+  ['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Not the answer we were expecting. Be sure to read carefully." }, DS, { s: "Without warning, Geoffrey turned and screamed at the waiter who had embarrassed him." }, Question, { q: "The waiter ___.", as: ["embarrassed Geoffrey", "felt embarrassed by Geoffrey"] }],
+  ['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Not the answer we were expecting. Be sure to read carefully." }, DS, { s: "The trainee talking to the barista lazily made a latte and didn't even try to make a design on top." }, Question, { q: "The trainee was probably ___.", as: ["not very attentive", "following procedure"] }],
+  ['shared-intro', Separator, { transfer: 1000, normalMessage: "", errorMessage: "Not the answer we were expecting. Be sure to read carefully." }, DS, { s: "Madame de Plessy sat up all night worrying about her son who was on the front lines." }, Question, { q: "Madame de Plessy was concerned about ___.", as: ["her son", "her husband"] }],
 
-['shared-intro', Message, {consentRequired: false,
-                   html: ["div",
-                           ["p", "Alright, that's it for practice! If you got any wrong, that's OK. Just be sure to read naturally but carefully, making sure you understand each word as you go. Press any key when you're ready to begin."]
-                        ]}],
+  ['shared-intro', Message, {
+    consentRequired: false,
+    html: ["div",
+      ["p", "Alright, that's it for practice! If you got any wrong, that's OK. Just be sure to read naturally but carefully, making sure you understand each word as you go. Press any key when you're ready to begin."]
+    ]
+  }],
 
-['shared-intro',"Separator",{transfer: 4000, normalMessage: "Hands in place! Your first sentence of this block will start soon."}],
+  ['shared-intro', "Separator", { transfer: 4000, normalMessage: "Hands in place! Your first sentence of this block will start soon." }],
 
-["timeoutSep", Separator, { transfer: 1000, normalMessage: "", errorMessage: "Not the answer we were expecting. Be sure to read carefully."}],
+  ["timeoutSep", Separator, { transfer: 1000, normalMessage: "", errorMessage: "Not the answer we were expecting. Be sure to read carefully." }],
 
-//// Shared experimental items + fillers
-[["pozzi-gram-sg-sg",1], DS, {s:"The gossip in the camp routinely angers the officer, because it would often contain intentional inaccuracies."},Question, {q: "The gossip was circulating ___.", as: ["in the camp","at the police station"]}],
-[["pozzi-gram-pl-sg",1], DS, {s:"The gossip in the camps routinely angers the officer, because it would often contain intentional inaccuracies."},Question, {q: "The gossip was circulating ___.", as: ["in the camp","at the police station"]}],
-[["pozzi-ungr-sg-sg",1], DS, {s:"The gossip in the camp routinely anger the officer, because it would often contain intentional inaccuracies."},Question, {q: "The gossip was circulating ___.", as: ["in the camp","at the police station"]}],
-[["pozzi-ungr-pl-sg",1], DS, {s:"The gossip in the camps routinely anger the officer, because it would often contain intentional inaccuracies."},Question, {q: "The gossip was circulating ___.", as: ["in the camp","at the police station"]}],
-[["pozzi-gram-sg-pl",1], DS, {s:"The gossip in the camp routinely angers the officer, because they would often contain intentional inaccuracies."},Question, {q: "The gossip was circulating ___.", as: ["in the camp","at the police station"]}],
-[["pozzi-gram-pl-pl",1], DS, {s:"The gossip in the camps routinely angers the officer, because they would often contain intentional inaccuracies."},Question, {q: "The gossip was circulating ___.", as: ["in the camp","at the police station"]}],
-[["pozzi-ungr-sg-pl",1], DS, {s:"The gossip in the camp routinely anger the officer, because they would often contain intentional inaccuracies."},Question, {q: "The gossip was circulating ___.", as: ["in the camp","at the police station"]}],
-[["pozzi-ungr-pl-pl",1], DS, {s:"The gossip in the camps routinely anger the officer, because they would often contain intentional inaccuracies."},Question, {q: "The gossip was circulating ___.", as: ["in the camp","at the police station"]}],
+  //// Shared experimental items + fillers
+  [["pozzi-gram-sg-sg", 1], DS, { s: "The gossip in the camp routinely angers the officer, because it would often contain intentional inaccuracies." }, Question, { q: "The gossip was circulating ___.", as: ["in the camp", "at the police station"] }],
+  [["pozzi-gram-pl-sg", 1], DS, { s: "The gossip in the camps routinely angers the officer, because it would often contain intentional inaccuracies." }, Question, { q: "The gossip was circulating ___.", as: ["in the camp", "at the police station"] }],
+  [["pozzi-ungr-sg-sg", 1], DS, { s: "The gossip in the camp routinely anger the officer, because it would often contain intentional inaccuracies." }, Question, { q: "The gossip was circulating ___.", as: ["in the camp", "at the police station"] }],
+  [["pozzi-ungr-pl-sg", 1], DS, { s: "The gossip in the camps routinely anger the officer, because it would often contain intentional inaccuracies." }, Question, { q: "The gossip was circulating ___.", as: ["in the camp", "at the police station"] }],
+  [["pozzi-gram-sg-pl", 1], DS, { s: "The gossip in the camp routinely angers the officer, because they would often contain intentional inaccuracies." }, Question, { q: "The gossip was circulating ___.", as: ["in the camp", "at the police station"] }],
+  [["pozzi-gram-pl-pl", 1], DS, { s: "The gossip in the camps routinely angers the officer, because they would often contain intentional inaccuracies." }, Question, { q: "The gossip was circulating ___.", as: ["in the camp", "at the police station"] }],
+  [["pozzi-ungr-sg-pl", 1], DS, { s: "The gossip in the camp routinely anger the officer, because they would often contain intentional inaccuracies." }, Question, { q: "The gossip was circulating ___.", as: ["in the camp", "at the police station"] }],
+  [["pozzi-ungr-pl-pl", 1], DS, { s: "The gossip in the camps routinely anger the officer, because they would often contain intentional inaccuracies." }, Question, { q: "The gossip was circulating ___.", as: ["in the camp", "at the police station"] }],
 ];
